feb61d370db231167f01380d480514a896789aa9
hima <himaanshuvasantham3@gmail.com>
2022-12-23 09:57:17 +0530

    fcomm

--- app.js
+++ app.js
@@ -6,21 +6,6 @@ const path = require("path");
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
-
-app.get("/", async (request, response) => {
-  const allTodos = await Todo.getTodos();
-  if (request.accepts("html")) {
-    response.render("index", {
-      allTodos,
-    });
-  } else {
-    response.json({
-      allTodos,
-    });
-  }
-});
-
-app.use(express.static(path.join(__dirname, "public")));
 // eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, "public")));
 
